aacdd97d389e683ae76db27fd1f3c30914afcfc8
JAEGEON LEE <worjs2964@gmail.com>
2023-03-15 17:34:32 +0900

    feat: Pinia 추가

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from "vue";
 import './style.css'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import pinia from "./stores";
 
 new Vue({
+    pinia,
     vuetify,
     render: (h) => h(App)
 }).$mount('#app');
