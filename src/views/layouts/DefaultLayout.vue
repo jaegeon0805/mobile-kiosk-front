8f80bbcb82e8a152454fb7feed7719e8cdb932eb
JAEGEON LEE <rebuild0805@gmail.com>
2023-03-17 15:12:01 +0900

    feat: DefaultLayout 구성 추가
    + 네비게이션바, 앱바, 메인, 푸터

--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-    <slot />
+    <v-navigation-drawer app v-model="navigation">
+      <!-- Add your navigation drawer content here -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="navigation = !navigation" />
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+
+    <v-footer app inset>
+      <v-spacer />
+      <div class="text-overline">
+        <span> Mobile-Kiosk </span>
+      </div>
+    </v-footer>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const navigation = ref(true);
+</script>
