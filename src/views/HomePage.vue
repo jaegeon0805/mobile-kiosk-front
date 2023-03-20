3654680f488bcafc268c9ee688ac5fc681031777
JAEGEON LEE <rebuild0805@gmail.com>
2023-03-20 22:19:55 +0900

    fix: HomePage profile 확인 API 제거
    테스트용 API이고 Response 형식이 달라져서 제거

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
-    test {{ profile }}
-    <span>{{ profile }}</span>
+    <span>Home Page..</span>
   </div>
 </template>
 
-<script setup lang="ts">
-import { getApi } from "@/utils/apis";
-import { onMounted, ref } from "vue";
-
-const profile = ref("");
-
-onMounted(async () => {
-  const response = await getApi<string>("profile/test");
-  if (response.success) {
-    profile.value = response.result;
-  }
-});
-</script>
+<script setup lang="ts"></script>
