b90aa54616391ea5c3a84bfdf2feb7869baf1259
JAEGEON LEE <rebuild0805@gmail.com>
2023-03-24 00:33:34 +0900

    refactor: vite.config.ts 필요없는 코드 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue2 from '@vitejs/plugin-vue2';
+import { defineConfig } from "vite";
+import vue2 from "@vitejs/plugin-vue2";
 import Components from "unplugin-vue-components/vite";
-import {VuetifyResolver} from "unplugin-vue-components/resolvers";
-import {resolve} from "node:path";
+import { VuetifyResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [
@@ -20,15 +20,4 @@ export default defineConfig({
   server: {
     port: 8081,
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     sass: {
-  //       additionalData: [
-  //         // vuetify variable overrides
-  //         '@import "@/styles/variables.scss"',
-  //         '',
-  //       ].join('\n'),
-  //     },
-  //   },
-  // },
-})
+});
