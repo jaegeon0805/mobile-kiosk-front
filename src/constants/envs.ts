9618a4b3f0b3ce8927fb072eba8a2a1ea6310229
JAEGEON LEE <rebuild0805@gmail.com>
2023-05-19 18:56:59 +0900

    feat: 환경 변수 추가

--- src/constants/envs.ts
+++ src/constants/envs.ts
@@ -1,6 +1,7 @@
 export default Object.freeze({
   API_HOST: import.meta.env.VITE_BASE_API_URL as string,
   SOCKET_URL: import.meta.env.VITE_SOCKET_URL as string,
+  FRONT_URL: import.meta.env.VITE_FRONT_URL as string,
   S3_BUCKET: import.meta.env.VITE_S3_BUCKET as string,
   DATETIME_FORMAT: import.meta.env.VITE_DATETIME_FORMAT as string,
 });
